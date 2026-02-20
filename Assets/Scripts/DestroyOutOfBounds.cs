be15f5ce5c7e3394313e094ddce2b1f589d09e65
Sami Samara <77996431+samisamara@users.noreply.github.com>
2026-02-20 01:49:51 -0600

    created animal prefabs, set all prefabs to be destroyed when reaching far enough off screen to reduce resource waste

---
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class DestroyOutOfBounds : MonoBehaviour
+{
+    [SerializeField] private float topBound = 30;
+    [SerializeField] private float lowerBound = -10;
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.z > topBound)
+        {
+            Destroy(gameObject);
+        } 
+        else if (transform.position.z < lowerBound) 
+        {
+            Destroy(gameObject);
+        }
+    }
+}
