88e827b3267b12a117a0798d149be49900d6725f
Sami Samara <77996431+samisamara@users.noreply.github.com>
2026-02-20 20:16:08 -0600

    worked on challenge 2 and bonus challenges

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    [SerializeField] private float dogTimer = 0.2f;
+    private float prevShot;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (Time.time - prevShot >= dogTimer)
+            {
+                Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+                prevShot = Time.time;
+            }
+            
+        }
+    }
+}
--- Assets/Scripts/DestroyOutOfBounds.cs
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -13,6 +13,7 @@ public class DestroyOutOfBounds : MonoBehaviour
         }
         else if (transform.position.z < lowerBound)
         {
+            Debug.Log("Game Over!");
             Destroy(gameObject);
         }
     }
